759dcbd69ffed2d91268ed2f216f2d967b437898
codewmanas <manas.kolaskar@somaiya.edu>
2025-02-18 12:11:54 +0530

    The APIxie

--- apixie/lib/main.dart
+++ apixie/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RAG-zilla',
+      title: 'APIxie',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.submitButton),
         scaffoldBackgroundColor: AppColors.background,
