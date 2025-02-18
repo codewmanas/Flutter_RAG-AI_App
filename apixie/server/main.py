09555e2a07956d5c21ff1ef77c937dd9cff9ce65
codewmanas <manas.kolaskar@somaiya.edu>
2025-02-18 14:41:43 +0530

    Search ChatEndpoint

--- apixie/server/main.py
+++ apixie/server/main.py
@@ -10,8 +10,14 @@ search_service = SearchService()
 # /chat?query=Who%20is%20Manas?%20.
 @app.post("/chat")
 def chat_endpoint(body: Chatbody):
+    
+
     # Search Web and Find Appropriate sources
-    search_service.web_search(body.query)
+    search_results = search_service.web_search(body.query)
+    print(search_results)
+
+
     # sort The Sources
+
     # Generate Response using the LLM
     return body.query
---
+++ apixie/server/services/sort_source_service.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class SortSourceService:
+    def sort_sources(self, query: str, search_results: List[dict] ):
+        pass
